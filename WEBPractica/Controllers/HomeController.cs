dbe0dbde68d5f24d5015e24de21e27813473b774
Lizeth <Lizeth@LAPTOP-C3VRP0N1>
2021-08-25 13:53:36 -0400

    Agregar archivos de proyecto.

---
+++ WEBPractica/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBPractica.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Lizeth Huayta Flores";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "hfl2022986@est.univalle.edu";
+
+            return View();
+        }
+    }
+}
---
+++ WEBPractica/Models/BDEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WEBPractica.Models
+{
+    public class BDEntities:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
